6d43c3d7febd46d0a4a912a10e93eeed340fad21
dleshem <dleshem@gmail.com>
2013-03-22 11:53:37 +0200

    Documentation.

--- zcredit-java-client/src/il/co/zcredit/CreditCardPayment.java
+++ zcredit-java-client/src/il/co/zcredit/CreditCardPayment.java
@@ -1,10 +1,24 @@
 package il.co.zcredit;
 
 public class CreditCardPayment {
+	/** Card number, e.g. "1111222233334444" */
 	public String number;
+	
+	/** Expiration month, 1-based. */
 	public Integer expireMonth;
+	
+	/** Expiration year (4 digits or 2 digits). */
 	public Integer expireYear;
+	
+	/**
+	 * Card Security Code (optional).
+	 * @see http://en.wikipedia.org/wiki/Card_security_code
+	 */
 	public String csc;
+	
+	/** Card holder-ID (optional). */
 	public String holderId;
+	
+	/** Amount to charge in ILS, e.g. 19.99 */
 	public Double amount;
 }
--- zcredit-java-client/src/il/co/zcredit/ZcreditClient.java
+++ zcredit-java-client/src/il/co/zcredit/ZcreditClient.java
@@ -4,6 +4,13 @@ import java.io.IOException;
 
 import com.google.api.client.http.HttpRequestFactory;
 
+/**
+ * A non-official zcredit.co.il web-client.
+ * 
+ * Works in two steps:
+ * 1) Creates a new authenticated browsing session with www.zcredit.co.il
+ * 2) Clears a credit payment using the in-site form.
+ */
 public class ZcreditClient {
 	private final HttpRequestFactory requestFactory;
 	private final Integer connectTimeout;
@@ -20,6 +27,10 @@ public class ZcreditClient {
 		this.password = password;
 	}
 	
+	/**
+	 * @param creditCardPayment   Credit-card payment to clear.
+	 * @return the payment's unique ID
+	 */
 	public String clearCreditCard(CreditCardPayment creditCardPayment) throws IOException, ZcreditException {
 		final Authenticator authenticator = new Authenticator(
 				requestFactory, connectTimeout, readTimeout, username, password);
